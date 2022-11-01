447d6e177e96f91ae323528010e300bd260217db
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2022-11-01 14:50:17 +0530

    Add files via upload

---
+++ declaration.java
@@ -0,0 +1,23 @@
+package javafiles;
+
+public class declaration {
+    
+    public static void main(String[] args) {
+        
+        int a = 10;
+        float b = 12;
+        double c = 10.5;
+        char d = 'S';
+        boolean e = true;
+        String f = "Alpha";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+
+    }
+}
