d882528aa027b24705b89b6a2644c079bafd1345
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2022-11-01 19:32:04 +0530

    Add files via upload

---
+++ printno.java
@@ -0,0 +1,20 @@
+package javafiles;
+
+public class printno {
+    public static void main(String[] args) {
+        
+        int i = 10;
+
+        while (i >= 0) {
+        System.out.println(i);
+        
+        i--;
+        }
+
+
+    
+
+
+    
+    }
+}
