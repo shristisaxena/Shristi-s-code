c4b8f92e4f458b9133cfea5000b8f0f9cbe20edb
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2022-11-01 19:24:21 +0530

    Add files via upload

---
+++ oddnumbers.java
@@ -0,0 +1,12 @@
+package javafiles;
+
+public class oddnumbers {
+    public static void main(String[] args) {
+        
+        
+        for(int i = 1; i <= 10; i = i+2) {
+            System.out.println(i);
+
+        }
+    }
+}
