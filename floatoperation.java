6ea0ef4fedcd88a370e9699d906417b6fad412bf
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2022-11-10 12:33:39 +0530

    Add files via upload

---
+++ floatoperation.java
@@ -0,0 +1,33 @@
+public class floatoperation {
+
+    public static void main(String[] args) {
+        
+         float num1 = 12.5F;
+         float num2 = 10.5F;
+
+         float sum = num1 + num2;
+         System.out.println(sum);
+
+         float difference  = num1 - num2;
+         System.out.println(difference);
+
+         float division = num1 / num2;
+         System.out.println(division);
+
+         float multiplication = num1 * num2;
+         System.out.println(multiplication);
+
+
+
+
+
+
+
+    
+
+          
+
+
+    }
+    
+}
