7e635db8f8cf4ab49c13330ea7d4daf17ea64bfd
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2022-11-20 15:40:00 +0530

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,22 +1,14 @@
-package com.greatlearrning.codingChallenge;
+package com.greatlearning.corejava;
 
 public class Customer {
-
-	String fullName; //small camel casing
-	int age;
-
-	public static void main(String[] args) {
-
-		Customer customer = new Customer();
-		customer.fullName = "Shristi Saxena";
-		customer.age = 22;
-		System.out.println(customer.fullName);
-		System.out.println(customer.age);
-
-
-
+	
+	private int age = 22;
+	public char letter = 's';
+	protected float feet = 12.6f;
+	
+	
 		// TODO Auto-generated method stub
 
 	}
 
-}
+
---
+++ CustomerDriver.java
@@ -0,0 +1,16 @@
+package com.greatlearning.corejava;
+
+public class CustomerDriver {
+
+	public static void main(String[] args) {
+		
+		Customer customer = new Customer();
+		System.out.println(customer.age);
+		System.out.println(customer.letter);
+		System.out.println(customer.feet);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
