07543f9c8dd8438142435c9147b20384332ee7d9
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2022-11-20 15:36:51 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,22 @@
+package com.greatlearrning.codingChallenge;
+
+public class Customer {
+
+	String fullName; //small camel casing
+	int age;
+
+	public static void main(String[] args) {
+
+		Customer customer = new Customer();
+		customer.fullName = "Shristi Saxena";
+		customer.age = 22;
+		System.out.println(customer.fullName);
+		System.out.println(customer.age);
+
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
