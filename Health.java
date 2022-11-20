cf5551681386d5ea8b8b64f5c9d73cef5952ed3d
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2022-11-20 15:39:13 +0530

    Add files via upload

---
+++ Health.java
@@ -0,0 +1,10 @@
+package com.greatlearning.corejava;
+
+public class Health {
+	protected int age = 23;
+	protected float weight = 50.5f;
+	protected float bmi = 23.6f;
+
+	// TODO Auto-generated method stub
+
+}
---
+++ HealthDriver.java
@@ -0,0 +1,14 @@
+
+// TODO Auto-generated method stub
+package com.greatlearning.corejava;
+
+public class HealthDriver {
+
+	public static void main(String[] args) {
+		Health health = new Health();
+		System.out.println("Age is : " + health.age + "years");
+		System.out.println("Weight is : " + health.weight + "KG");
+		System.out.println("BMI is : " + health.bmi + "KG");
+	}
+
+}
